b7b5d3b6c0c89974b31329cec84b55fab33e9a5a
unknown <jeff@jeffhq.com>
2020-01-18 15:41:15 -0800

    Potentially fix slate frame

--- python/tk_nuke_quickreview/dialog.py
+++ python/tk_nuke_quickreview/dialog.py
@@ -362,7 +362,7 @@ class Dialog(QtGui.QWidget):
 
         # get frame ranges from ui
         try:
-            start_frame = int(self.ui.start_frame.text())
+            start_frame = (int(self.ui.start_frame.text())-1)
             end_frame = int(self.ui.end_frame.text())
         except Exception, e:
             raise ValueError("Could not determine frame range values from UI.")
