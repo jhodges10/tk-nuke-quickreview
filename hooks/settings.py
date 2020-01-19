8e940f0de41d26cee02ac656020a3fca8cb3419e
unknown <jeff@jeffhq.com>
2020-01-18 16:04:27 -0800

    Reduce quality further

--- hooks/settings.py
+++ hooks/settings.py
@@ -140,10 +140,10 @@ class Settings(HookBaseClass):
         """
         write_node["file_type"].setValue("mov")
         write_node["mov64_codec"].setValue("ap4h")
-        write_node["mov64_bitrate"].setValue(5000)
-        write_node["mov64_bitrate_tolerance"].setValue(40000000)
-        write_node['mov64_quality_min'].setValue(2)
-        write_node["mov64_quality_max"].setValue(20)
+        write_node["mov64_bitrate"].setValue(4000)
+        write_node["mov64_bitrate_tolerance"].setValue(8000)
+        write_node['mov64_quality_min'].setValue(5)
+        write_node["mov64_quality_max"].setValue(15)
         write_node["mov64_gop_size"].setValue(12)
         write_node["mov64_b_frames"].setValue(0)
 
